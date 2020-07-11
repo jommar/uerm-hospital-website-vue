27d2adf42ca5f18934d5632e290ec665a730a9a4
Jommar Ilagan <jom102386@gmail.com>
2020-07-11 10:21:25 +0800

    refactored data variables

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
     <main>
       <Header v-bind:links="headerLinks" v-bind:changeRoute="changeRoute" />
       <div v-if="route == '/'">
-        <Carousel v-bind:carousel="carousel" />
+        <Carousel />
         <Temp />
       </div>
 
       <div v-if="route == '/doctors'">
-        <Doctors v-bind:doctors="doctors" v-bind:isDoctorsLoaded="isDoctorsLoaded" v-bind:filterDoctors="filterDoctors" v-bind:alphabet="alphabet" />
+        <!-- <Doctors v-bind:doctors="doctors" v-bind:isDoctorsLoaded="isDoctorsLoaded" v-bind:filterDoctors="filterDoctors" v-bind:alphabet="alphabet" /> -->
+        <Doctors v-bind:apiKey="apiKey" />
       </div>
       <Footer />
     </main>
@@ -30,11 +31,7 @@ export default {
   data(){
     return {
       apiKey:'eSWHugHzUmZQ2GUsBKffyNKeNZHuSWtX',
-      doctors:[],
-      doctorsAll:[],
-      isDoctorsLoaded:false,
       route:window.location.pathname,
-      alphabet:['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
       headerLinks:[
         [
           {href:'/',title:'Home',},
@@ -51,47 +48,14 @@ export default {
           {href:'/doctor/specialty',title:'Per Doctor / Specialty',},
         ],
       ],
-      carousel:[
-        {backgroundImage:'url(assets/img-temp/1920x1080/img5.jpg)',title:'Lorem Ipsum',content:'Cras vitae purus sit amet ipsum egestas vestibulum. Nam euismod nisl at magna mattis, quis mattis odio bibendum. Phasellus elementum sodales mauris, at scelerisque mi eleifend efficitur.',},
-        {backgroundImage:'url(assets/img-temp/1920x1080/img3.jpg)',title:'Lorem Ipsum',content:'Cras vitae purus sit amet ipsum egestas vestibulum. Nam euismod nisl at magna mattis, quis mattis odio bibendum. Phasellus elementum sodales mauris, at scelerisque mi eleifend efficitur.',},
-        {backgroundImage:'url(assets/img-temp/1920x1080/img1.jpg)',title:'Lorem Ipsum',content:'Cras vitae purus sit amet ipsum egestas vestibulum. Nam euismod nisl at magna mattis, quis mattis odio bibendum. Phasellus elementum sodales mauris, at scelerisque mi eleifend efficitur.',},
-      ],
     };
   },
   methods:{
-    async fetchDoctors(){
-      const url = `https://apps.uerm.edu.ph:3443/doctors?auth=${this.apiKey}`;
-      const response = await fetch(
-        url,
-        {method:'GET'}
-      );
-      const responseJson = await response.json();
-
-      this.doctorsAll = responseJson.doctors;
-      this.doctors = this.doctorsAll;
-      this.isDoctorsLoaded = true;
-    },
     changeRoute(route){
       this.route = route;
       window.location.pathname = this.route;
     },
-    filterDoctors(letter){
-      if(letter == ''){
-        this.doctors = this.doctorsAll;
-        return;
-      }
-      this.doctors = this.doctorsAll.filter((doctor)=>{return doctor.fullName.substring(0,1) == letter});
-    },
   },
-  mounted(){
-    // this.fetchDoctors();
-    switch(this.route){
-      case '/doctors':
-        this.fetchDoctors();
-        break;
-    }
-    console.log(this.route);
-  }
 }
 </script>
 
