1389feb6436e92bc0bdf6ee680f0f7cba8db854b
Jommar Ilagan <jom102386@gmail.com>
2020-07-13 14:53:29 +0800

    updated icons

--- src/components/Doctors.vue
+++ src/components/Doctors.vue
@@ -4,50 +4,61 @@
       <h1 class="h1 mb-3 text-center">Our Doctors</h1>
     </div>
     <div class="g-bg-main py-3">
-      <div class="container">
-        <div class="my-3">
-          <img class="w-100" style="height:280px; object-fit: cover;" src="assets/img/stock/doctors.jpg" alt="doctors" />
-        </div>
-      </div>
+      <img
+        class="w-100"
+        id="docImg"
+        style="object-fit: cover;"
+        src="assets/img/stock/doctors.jpg"
+        alt="doctors"
+      />
     </div>
     <div class="container">
-      <div v-if="doctors.length == 0 && !isDoctorsLoaded" class="p-3 text-center">
+      <div
+        v-if="doctors.length == 0 && !isDoctorsLoaded"
+        class="p-3 text-center"
+      >
         <div>Fetching doctors info</div>
         <div>
           <b class="fa fa-spinner fa-spin"></b>
         </div>
       </div>
       <div class="text-center my-3" v-if="isDoctorsLoaded">
-        <span class="btn btn-outline-primary m-2" v-bind:class="{active:curentLetter == ''}" v-on:click="filterDoctors('')">All</span>
         <span
           class="btn btn-outline-primary m-2"
-          v-bind:class="{active:curentLetter == letter}"
+          v-bind:class="{ active: curentLetter == '' }"
+          v-on:click="filterDoctors('')"
+          >All</span
+        >
+        <span
+          class="btn btn-outline-primary m-2"
+          v-bind:class="{ active: curentLetter == letter }"
           v-on:click="filterDoctors(letter)"
           v-bind:key="key"
-          v-for="(letter,key) in alphabet"
-        >{{letter}}</span>
+          v-for="(letter, key) in alphabet"
+          >{{ letter }}</span
+        >
       </div>
       <div v-if="doctors.length > 0 && isDoctorsLoaded">
-        <div v-bind:key="key" v-for="(doctor,key) in doctors">
+        <div v-bind:key="key" v-for="(doctor, key) in doctors">
           <div class="card mb-3 p-3 g-bg-secondary-dark-v2">
-            <h3>{{doctor.fullName}}</h3>
+            <h3>{{ doctor.fullName }}</h3>
             <div class="row mb-3">
               <div class="col-md-3">
                 <div class="h4">Area of Specialty</div>
-                <div>{{doctor.specialty}}</div>
+                <div>{{ doctor.specialty }}</div>
               </div>
               <div class="col">
                 <span class="mx-2">
                   <b>Schedule:</b>
-                  {{doctor.sched}}
+                  {{ doctor.sched }}
                 </span>
                 <span class="mx-2">
                   <b>Room:</b>
-                  {{doctor.room}}
+                  {{ doctor.room }}
                 </span>
                 <span class="mx-2">
                   <b>Contact No:</b>
-                  {{doctor.contactNo}}
+                  {{ doctor.contactNo }}
                 </span>
               </div>
             </div>
@@ -56,10 +67,10 @@
                 <div class="h4">HMO Accreditation</div>
                 <div
                   v-bind:key="hmoKey"
-                  v-for="(hmo,hmoKey) in doctor.hmo.split(',')"
+                  v-for="(hmo, hmoKey) in doctor.hmo.split(',')"
                   class="badge badge-primary mx-1"
                 >
-                  <span>{{hmo}}</span>
+                  <span>{{ hmo }}</span>
                 </div>
               </div>
             </div>
@@ -75,10 +86,38 @@ export default {
   props: ["apiKey"],
   data() {
     return {
+      height: window.innerHeight > window.innerWidth ? "30vh" : "280px",
       doctors: [],
       isDoctorsLoaded: false,
-      alphabet: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-      curentLetter:'',
+      alphabet: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+      ],
+      curentLetter: "",
     };
   },
   methods: {
@@ -97,17 +136,27 @@ export default {
         this.doctors = this.doctorsAll;
         return;
       }
-      this.doctors = this.doctorsAll.filter(doctor => {
+      this.doctors = this.doctorsAll.filter((doctor) => {
         return doctor.fullName.substring(0, 1) == letter;
       });
-    }
+    },
   },
   mounted() {
     this.fetchDoctors();
     // console.log(this.apiKey);
-  }
+  },
 };
 </script>
 
 <style scoped>
+  @media screen and (orientation:portrait) {
+    #docImg{
+      height: 200px;
+    }
+  }
+  @media screen and (orientation:landscape) {
+    #docImg{
+      height: 35vh;
+    }
+  }
 </style>
